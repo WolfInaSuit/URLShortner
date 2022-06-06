11e601f9b38c827b4dcf1bfe8aa648b1e21ba53b
Stevie Ferro <Stevferro1@gmail.com>
2022-06-05 23:47:46 -0400

    Initial Commit

---
+++ URLShortner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  URLShortner
+//
+//  Created by Stevie Cassh on 6/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ URLShortner/URLShortnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLShortnerApp.swift
+//  URLShortner
+//
+//  Created by Stevie Cassh on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct URLShortnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
